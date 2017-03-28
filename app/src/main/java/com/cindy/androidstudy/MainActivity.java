879481bbb78ad03e827b216050161b1ce1523143
Shin Jiyoung <jiyoung@Jiui-MacBook-Pro.local>
2017-03-28 16:09:16 +0900

    FAB clans 라이브러리 사용법

--- app/src/main/java/com/cindy/androidstudy/MainActivity.java
+++ app/src/main/java/com/cindy/androidstudy/MainActivity.java
@@ -3,11 +3,11 @@ package com.cindy.androidstudy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import com.cindy.androidstudy.dialog.DialogActivity;
+import com.cindy.androidstudy.fab.FABActivity;
+import com.cindy.androidstudy.fab.FABLibActivity;
 import com.cindy.androidstudy.surfaceview.SurfaceViewActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
             case R.id.btn_floating_action_button : {
-                intent.setClass(this, FloatingActionButtonActivity.class);
+                intent.setClass(this, FABActivity.class);
+                //intent.setClass(this, FABLibActivity.class);
                 startActivity(intent);
                 break;
             }
---
+++ app/src/main/java/com/cindy/androidstudy/fab/FABLibActivity.java
@@ -0,0 +1,15 @@
+package com.cindy.androidstudy.fab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cindy.androidstudy.R;
+
+public class FABLibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab_lib);
+    }
+}
